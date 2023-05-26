9ec98d9aa60bd39521ba315a798180e8c49a3d3c
Esztellar <n1lvg0k@gmail.com>
2023-05-26 23:27:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Erettsegi_feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
